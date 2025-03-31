39d1531ffab4691c182d19dbee0e3234578b2395
Oluwa-ios <50711478+oluwatobiHammed@users.noreply.github.com>
2025-03-31 18:06:54 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "Koloda",
+    products: [
+        .library(
+            name: "Koloda",
+            targets: ["Koloda"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Koloda",
+            dependencies: []),
+        .testTarget(
+            name: "KolodaTests",
+            dependencies: ["Koloda"]),
+    ]
+)
